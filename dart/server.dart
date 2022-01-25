ea0fb590a2686f0fab8b72231933162e3fb83046
JoelNickson <joel2000nickson@gmail.com>
2022-01-25 23:27:21 +0530

    moved server.dart form /dart to /Server folder

--- dart/server.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main(utf8) {
-  ServerSocket.bind('127.0.0.1', 4041).then((serverSocket) {
-    serverSocket.listen((socket) {
-      socket.transform(utf8.decoder).listen(print);
-    });
-  });
-}
