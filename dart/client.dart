354752842126622c073d4bc6c6edd753e3dd7873
JoelNickson <joel2000nickson@gmail.com>
2022-01-25 22:14:25 +0530

    constanly listening client ready

--- dart/client.dart
+++ dart/client.dart
@@ -10,21 +10,7 @@ import 'dart:io';
 import 'dart:convert';
 import 'dart:async';
 
-void help_function() {
-  print('''
-    Commands available are:
-        - create (filename)
-        - cat (filename)
-        - edit (filename)
-        - delete (filename)
-        ''');
-}
-
 main() async {
-  var help = <String>{'!help', '--help', '-h', '!h', '/h'};
-  var commands = <String>{'edit', 'create', 'cat', 'delete'};
-  commands.addAll(help);
-
   Socket socket = await Socket.connect('127.0.0.1', 8000);
   print('connected');
 
@@ -36,18 +22,7 @@ main() async {
   String message = '';
   while (message != '#') {
     message = stdin.readLineSync() ?? '';
-    if (message != '') {
-      if (help.contains(message)) {
-        help_function();
-      } else if (commands.contains(message)) {
-        socket.write(message);
-      } else {
-        print('Command not found');
-      }
-    }
-
     socket.write(message);
-    // print(message);
   }
 
   // .. and close the socket
